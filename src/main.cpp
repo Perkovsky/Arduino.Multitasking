669c197faf758e9c617de8ba171120319c85786b
Ihor Perkovskyi <igor.v.perkovsky@gmail.com>
2023-03-25 14:35:48 -0400

    Add Buzzer

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@
 
 const int LED_RED_PIN = 7;
 const int LED_BLUE_PIN = 8;
+const int BUZZER_PIN = 12;
 
 Thread redLedThread = Thread();
 Thread blueLedThread = Thread();
+Thread buzzerThread = Thread();
 
 // Define the LED RED thread function
 void redLedThreadFunction() {
@@ -22,6 +24,35 @@ void blueLedThreadFunction() {
   digitalWrite(LED_BLUE_PIN, blueLedState ? HIGH : LOW);
 }
 
+// Define the BUZZER thread function
+void buzzerThreadFunction() {
+  // commented code has been transformed to correct (without delay())
+  //
+  // noTone(BUZZER_PIN);
+  // tone(BUZZER_PIN, 440, 200);
+  // delay(300);
+  //
+  // noTone(BUZZER_PIN);
+  // tone(BUZZER_PIN, 494, 500);
+  // delay(300);
+
+  // 1st solution
+  const int notes[] = {440, 300};
+  const int noteDurations[] = {200, 300};
+  static int noteIndex = 0;
+  static unsigned long lastNoteTime = 0;
+  unsigned long currentTime = millis();
+  
+  if (currentTime - lastNoteTime >= noteDurations[noteIndex]) {
+    lastNoteTime = currentTime;
+    tone(BUZZER_PIN, notes[noteIndex], noteDurations[noteIndex]);
+    noteIndex = (noteIndex + 1) % 2;
+  }
+  // else if (currentTime - lastNoteTime >= 100 - noteDurations[noteIndex]) {
+  //   noTone(BUZZER_PIN);
+  // }
+}
+
 void setup() {
   // Set the LED pins as outputs
   pinMode(LED_RED_PIN, OUTPUT);
@@ -32,10 +63,13 @@ void setup() {
   redLedThread.setInterval(100);
   blueLedThread.onRun(blueLedThreadFunction);
   blueLedThread.setInterval(1000);
+  buzzerThread.onRun(buzzerThreadFunction);
+  buzzerThread.setInterval(300);
 
   // Start the threads
   redLedThread.enabled = true;
   blueLedThread.enabled = true;
+  buzzerThread.enabled = true;
 }
 
 void loop() {
@@ -44,4 +78,7 @@ void loop() {
 
   if (blueLedThread.shouldRun())
 		blueLedThread.run();
+
+  if (buzzerThread.shouldRun())
+		buzzerThread.run();
 }
