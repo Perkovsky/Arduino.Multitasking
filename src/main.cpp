b82047fb0252d032d1e07201c182886ea4c6bd59
Ihor Perkovskyi <igor.v.perkovsky@gmail.com>
2023-03-25 14:03:29 -0400

    Use ArduinoThread

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,47 @@
 #include <Arduino.h>
-#include <TaskScheduler.h>
+#include <Thread.h>
+#include "ThreadController.h"
 
 const int LED_RED_PIN = 7;
 const int LED_BLUE_PIN = 8;
 
-Scheduler scheduler;
+Thread redLedThread = Thread();
+Thread blueLedThread = Thread();
 
-void blinkLedRed() {
-  digitalWrite(LED_RED_PIN, !digitalRead(LED_RED_PIN));
+// Define the LED RED thread function
+void redLedThreadFunction() {
+  static bool redLedState = false;
+  redLedState = !redLedState;
+  digitalWrite(LED_RED_PIN, redLedState ? HIGH : LOW);
 }
 
-void blinkLedBlue() {
-  digitalWrite(LED_BLUE_PIN, !digitalRead(LED_BLUE_PIN));
+// Define the LED BLUE thread function
+void blueLedThreadFunction() {
+  static bool blueLedState = false;
+  blueLedState = !blueLedState;
+  digitalWrite(LED_BLUE_PIN, blueLedState ? HIGH : LOW);
 }
-
-Task taskBlinkLedRed(100, TASK_FOREVER, &blinkLedRed);
-Task taskBlinkLedBlue(1000, TASK_FOREVER, &blinkLedBlue);
 
 void setup() {
+  // Set the LED pins as outputs
   pinMode(LED_RED_PIN, OUTPUT);
   pinMode(LED_BLUE_PIN, OUTPUT);
 
-  scheduler.addTask(taskBlinkLedRed);
-  scheduler.addTask(taskBlinkLedBlue);
-  scheduler.enableAll();
+  // Set up the threads
+  redLedThread.onRun(redLedThreadFunction);
+  redLedThread.setInterval(100);
+  blueLedThread.onRun(blueLedThreadFunction);
+  blueLedThread.setInterval(1000);
+
+  // Start the threads
+  redLedThread.enabled = true;
+  blueLedThread.enabled = true;
 }
 
 void loop() {
-  scheduler.execute();
+  if (redLedThread.shouldRun())
+		redLedThread.run();
+
+  if (blueLedThread.shouldRun())
+		blueLedThread.run();
 }
