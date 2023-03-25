7bf4f55b7dcdb7831087dce6fd3f92e162892a4e
Ihor Perkovskyi <igor.v.perkovsky@gmail.com>
2023-03-25 12:36:36 -0400

    FreeRTOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,49 @@
 #include <Arduino.h>
-#include <TaskScheduler.h>
+#include <Arduino_FreeRTOS.h>
 
 const int LED_RED_PIN = 7;
 const int LED_BLUE_PIN = 8;
 
-Scheduler scheduler;
-
-void blinkLedRed() {
-  digitalWrite(LED_RED_PIN, !digitalRead(LED_RED_PIN));
+void task1(void *parameters) {
+  digitalWrite(LED_RED_PIN, HIGH);
+  vTaskDelay(100 / portTICK_PERIOD_MS);
+  digitalWrite(LED_RED_PIN, LOW);
+  vTaskDelay(100 / portTICK_PERIOD_MS);
 }
 
-void blinkLedBlue() {
-  digitalWrite(LED_BLUE_PIN, !digitalRead(LED_BLUE_PIN));
+void task2(void *parameters) {
+  digitalWrite(LED_BLUE_PIN, HIGH);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  digitalWrite(LED_BLUE_PIN, LOW);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
-
-Task taskBlinkLedRed(100, TASK_FOREVER, &blinkLedRed);
-Task taskBlinkLedBlue(1000, TASK_FOREVER, &blinkLedBlue);
 
 void setup() {
   pinMode(LED_RED_PIN, OUTPUT);
   pinMode(LED_BLUE_PIN, OUTPUT);
 
-  scheduler.addTask(taskBlinkLedRed);
-  scheduler.addTask(taskBlinkLedBlue);
-  scheduler.enableAll();
+  xTaskCreate(
+    task1, // function name
+    "Task 1", // task name
+    1000, // stack size
+    NULL, // task parameters
+    1, // task priority
+    NULL // task handle
+  );
+
+  // Doen't work for Arduino Uno if try to use both tasks.
+  // Works only one of them.
+
+  // xTaskCreate(
+  //   task2, // function name
+  //   "Task 2", // task name
+  //   1000, // stack size
+  //   NULL, // task parameters
+  //   1, // task priority
+  //   NULL // task handle
+  // );
 }
 
 void loop() {
-  scheduler.execute();
+  
 }
